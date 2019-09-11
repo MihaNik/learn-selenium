fce20d79eee6a14b140508311d9a7767357de685
Михаил Коляда <mihail.kolyada@gmail.com>
2019-09-11 18:14:59 +0300

    change page assertion

--- src/test/java/LoginExampleTest.java
+++ src/test/java/LoginExampleTest.java
@@ -12,6 +12,7 @@ public class LoginExampleTest {
     private WebDriver driver;
     private WebDriverWait wait;
     private String pageUrl = "http://localhost/litecart/admin/";
+    private String platformVersion  = "LiteCart® 1.3.7";
 
     @BeforeMethod
     public void start (){
@@ -23,9 +24,14 @@ public class LoginExampleTest {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
-        String url = driver.findElement(By.xpath("//div[@class='logotype']/a")).getAttribute("href");
+/*        String url = driver.findElement(By.xpath("//div[@class='logotype']/a")).getAttribute("href");
         System.out.println(url);
-        Assert.assertEquals(url, pageUrl);
+        Assert.assertEquals(url, pageUrl);*/
+
+        String platform = driver.findElement(By.id("platform")).getText();
+        System.out.println(platform);
+        Assert.assertEquals(platform, platformVersion);
+
     }
 
     @AfterMethod
