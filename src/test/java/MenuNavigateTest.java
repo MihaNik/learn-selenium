244aad61b4c343cfe1f8dd3d8b68c2f3ce8ad0b6
Михаил Коляда <mihail.kolyada@gmail.com>
2019-09-19 12:21:36 +0300

    some changes

--- src/test/java/MenuNavigateTest.java
+++ src/test/java/MenuNavigateTest.java
@@ -20,10 +20,7 @@ public class MenuNavigateTest {
     private WebDriverWait wait;
 
     private boolean areElementsPresent(WebDriver driver, By locator) {
-        System.out.println("Test");
-        int size = driver.findElements(locator).size();
-        System.out.println(size);
-        return size > 0;
+        return driver.findElements(locator).size() > 0;
     }
 
     @BeforeMethod
@@ -37,53 +34,18 @@ public class MenuNavigateTest {
     }
 
     @Test
-    public void myFirstTest() {
+    public void menuNavigate() {
         driver.get("http://localhost/litecart/admin/login.php");
+
+        //login with app
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
         Assert.assertTrue(areElementsPresent(driver, By.cssSelector("ul#box-apps-menu")));
 
-        List<String> menuNames = driver.findElements(By.cssSelector("ul#box-apps-menu span.name"))
-                .stream()
-                .map(WebElement -> WebElement.getText())
-                .collect(Collectors.toList());
-        System.out.println(menuNames);
 
-
-        driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).get(1).click();
-        long startTime = System.currentTimeMillis();
-        boolean count2 = driver.findElements(By.cssSelector("ul.docs li")).size()>0;
-        driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).get(0).click();
-        long stopTime = System.currentTimeMillis();
-        long elapsedTime = stopTime - startTime;
-        System.out.println(elapsedTime);
-
-
-
-        driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).get(2).click();
-        long startTime1 = System.currentTimeMillis();
-        boolean count1= driver.findElements(By.cssSelector("ul.docs li")).size()>0;
-        driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).get(0).click();
-        long stopTime1 = System.currentTimeMillis();
-        long elapsedTime1 = stopTime - startTime;
-        System.out.println(elapsedTime1);
-
-
-/*        String id = driver.findElements(By.cssSelector("ul.docs li")).get(0).getAttribute("id");
-        System.out.println(id);*/
-
-
-/*        for (WebElement el : driver.findElements(By.cssSelector("ul.docs a"))
-        ) {
-            el.click();
-            Assert.assertTrue(areElementsPresent(driver, By.cssSelector("td#content h1")));
-        }*/
-
-        // driver.findElements(By.cssSelector("ul.docs a")).forEach(WebElement::click);
-
-
-/*        for (int i = 0; i < driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).size(); i++) {
+        //menu navigation
+        for (int i = 0; i < driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).size(); i++) {
             driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).get(i).click();
             if (driver.findElements(By.cssSelector("ul.docs li")).size() > 0) {
                 for (int j = 0; j < driver.findElements(By.cssSelector("ul.docs a")).size(); j++) {
@@ -97,14 +59,16 @@ public class MenuNavigateTest {
                 System.out.println(driver.findElement(By.cssSelector("td#content h1")).getText());
 
             }
-        }*/
+        }
+
+
 
 
     }
 
     @AfterMethod
     public void stop() {
-        //driver.quit();
-        //driver = null;
+        driver.quit();
+        driver = null;
     }
 }
